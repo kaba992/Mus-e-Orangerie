745e6789dc7201f00de0b55d470287e42456c16d
ibrahima kalil kaba <ikkaba92@hotmail.fr>
2023-01-09 11:29:31 +0100

    garage 2

--- src/Experience/World/scenes/garage.js
+++ src/Experience/World/scenes/garage.js
@@ -11,17 +11,29 @@ export default class Garage {
         this.scene = this.experience.scene
         this.resources = this.experience.resources
         this.camera = this.experience.camera.instance
+        this.camera.position.set(20.5,8.21,23)
+        this.debug = this.experience.debug
         this.resource = this.resources.items.garage
         this.addGarage()
+
+        if (this.debug.active) {
+            this.debugFolder = this.debug.ui.addFolder('camera')
+
+            this.debugFolder.add(this.camera.position, 'x').min(-50).max(50).step(0.01).name('positionX')
+            this.debugFolder.add(this.camera.position, 'y').min(-50).max(50).step(0.01).name('positionY')
+            this.debugFolder.add(this.camera.position, 'z').min(-50).max(50).step(0.01).name('positionZ')
+        }
+
 
     }
 
     addGarage() {
-       
+
         this.garage = this.resource.scene
         console.log(this.garage);
         this.garage.scale.set(1, 1, 1)
-        this.garage.position.set(0, 0, 0)
+        this.garage.position.set(0, -2, 0)
+        
         this.scene.add(this.garage)
 
     }
