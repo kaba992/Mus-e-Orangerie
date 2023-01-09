65d336dd708e3a0db90c432bc95f2b5d148ee23d
kaba992 <ikkaba92@hotmail.fr>
2023-01-09 19:47:06 +0100

    resolve

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -17,17 +17,30 @@ export default class Camera
 
     setInstance()
     {
-        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
-        this.instance.position.set(6, 4, 8)
+        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 300)
+        this.instance.position.set(0, 35, -10)
+        this.instance.lookAt(0,0,0)
         this.scene.add(this.instance)
     }
 
     setControls()
     {
         this.controls = new OrbitControls(this.instance, this.canvas)
-        this.controls.enableDamping = true
-    }
+        this.controls.screenSpacePanning = false; // pan orthogonal to world-space direction camera.up
 
+        // this.controls.mouseButtons.LEFT = THREE.MOUSE.PAN;
+        // this.controls.mouseButtons.RIGHT = THREE.MOUSE.ROTATE;
+        // this.controls.touches.ONE = THREE.TOUCH.PAN;
+        // this.controls.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
+        //
+        // this.controls.enableDamping = true
+        // this.controls.enablePan = true;
+        // this.controls.minDistance = 10
+        // this.controls.maxDistance = 35
+        // this.controls.maxPolarAngle = Math.PI/2 -Math.PI/10
+        // this.controls.minPolarAngle = Math.PI / 8
+        // this.controls.enableRotate = false;
+    }
     resize()
     {
         this.instance.aspect = this.sizes.width / this.sizes.height
